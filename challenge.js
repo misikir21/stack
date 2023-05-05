dcccc67456da6e491e6f604b8206707f77668543
misikira <misikirteka@gmail.com>
2023-05-05 06:23:16 -0700

    first attempt

--- challenge.js
+++ challenge.js
@@ -1,15 +1,62 @@
 // Optionally: start with your code from LinkedList challenge.
 
+// program to implement stack data structure
 class Stack {
-  push(number) {
-    // your code here
-  }
-  
-  pop() {
-    // your code here
-  }
+    constructor() {
+        this.items = [];
+    }
+    
+    // add element to the stack
+    add(element) {
+        return this.items.push(element);
+    }
+    
+    // remove element from the stack
+    remove() {
+        if(this.items.length > 0) {
+            return this.items.pop();
+        }
+    }
+    
+    // view the last element
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+    
+    // check if the stack is empty
+    isEmpty(){
+       return this.items.length == 0;
+    }
+   
+    // the size of the stack
+    size(){
+        return this.items.length;
+    }
+ 
+    // empty the stack
+    clear(){
+        this.items = [];
+    }
 }
 
+let stackk = new Stack();
+stackk.add(1);
+stackk.add(2);
+stackk.add(4);
+stackk.add(8);
+console.log(stackk.items);
+
+stackk.remove();
+console.log(stackk.items);
+
+console.log(stackk.peek());
+
+console.log(stack.isEmpty());
+
+console.log(stack.size());
+
+stack.clear();
+console.log(stack.items);
 const stack = new Stack()
 stack.push(3)
 stack.push(5)
