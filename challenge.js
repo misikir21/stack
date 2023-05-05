37cba2ffe0a1483d5cf52699305f1706d3159aba
misikira <misikirteka@gmail.com>
2023-05-05 06:27:16 -0700

    first attempt

--- challenge.js
+++ challenge.js
@@ -1,77 +1,31 @@
 // Optionally: start with your code from LinkedList challenge.
 
-// program to implement stackk data structure
 class Stack {
-    constructor() {
-        this.items = [];
-    }
-    
-    // add element to the stackk
-    add(element) {
-        return this.items.push(element);
-    }
-    
-    // remove element from the stackk
-    remove() {
-        if(this.items.length > 0) {
-            return this.items.pop();
-        }
-    }
-    
-    // view the last element
-    peek() {
-        return this.items[this.items.length - 1];
-    }
-    
-    // check if the stackk is empty
-    isEmpty(){
-       return this.items.length == 0;
-    }
-   
-    // the size of the stackk
-    size(){
-        return this.items.length;
-    }
- 
-    // empty the stackk
-    clear(){
-        this.items = [];
-    }
+  push(number) {
+    // your code here
+    return this.number.push(number);
+  }
+  
+  pop() {
+    // your code here
+  }
 }
 
-let stackk = new Stack();
-stackk.add(1);
-stackk.add(2);
-stackk.add(4);
-stackk.add(8);
-console.log(stackk.items);
-
-stackk.remove();
-console.log(stackk.items);
-
-console.log(stackk.peek());
-
-console.log(stackk.isEmpty());
-
-console.log(stackk.size());
-
-stackk.clear();
-console.log(stackk.items);
-const stackk = new Stack()
-stackk.push(3)
-stackk.push(5)
-console.log(stackk.pop())
+const stack = new Stack()
+stack.push(3)
+stack.push(5)
+console.log(stack.pop())
 // => 5
 
-stackk.push(2)
-stackk.push(7)
-console.log(stackk.pop())
+stack.push(2)
+stack.push(7)
+console.log(stack.pop())
 // => 7
 
-console.log(stackk.pop())
+console.log(stack.pop())
 // => 2
 
-console.log(stackk.pop())
+console.log(stack.pop())
 // => 3
 
 module.exports = Stack
